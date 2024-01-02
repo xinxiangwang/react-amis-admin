610f8ed0a6c10985d33c328da95988ee253c5a7c
wxx <1104642244@qq.com>
2024-01-02 22:36:34 +0800

    table组件 自定义行内容

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
-	console.log('czxczxc')
 	const jump = useNavigate()
 	const onJump = () => {
 		jump('/user')
--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -1,5 +1,34 @@
+import useAmisRender from '@/hooks/useAmisRender'
+
 function User() {
-	return <h1>User</h1>
+	const { renderSchema } = useAmisRender()
+	return renderSchema({
+		type: 'page',
+		body: {
+			type: 'table',
+			tableLayout: 'fixed',
+			data: {
+				items: [
+					{ id: 1, engine: 1 },
+					{ id: 2, engine: 2 }
+				]
+			},
+			columns: [
+				{
+					name: 'id',
+					label: 'ID'
+				},
+				{
+					name: 'engine',
+					component: (params) => {
+						console.log(params)
+						return <h1>123</h1>
+					},
+					label: 'Rendering engine'
+				}
+			]
+		}
+	})
 }
 
 export default User
